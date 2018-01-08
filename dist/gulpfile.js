4b9abf0173cee394ed72a8494849af31852c9e04
SAPIENT\uupad3 <uupadhyay3@sapient.com>
2018-01-08 17:10:41 +0530

    Added mocha task

--- dist/gulpfile.js
+++ dist/gulpfile.js
@@ -2,6 +2,10 @@ var gulp = require('gulp')
 var babel = require('gulp-babel');
 var mocha = require('gulp-mocha');
 var log = require('gulplog');
+var sass = require('gulp-sass');            //compiles SASS
+var minifyCss = require('gulp-minify-css'); //minifies css
+var concat = require('gulp-concat');        //joins multiple files into one
+var rename = require('gulp-rename');        //renames files
 
 gulp.task('es6', () => {
   return gulp.src('./*.js')
