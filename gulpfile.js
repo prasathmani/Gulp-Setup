3fff586b615c6f1eb60d3711be708012c3194729
Prasath Mani <pmani@sapient.com>
2018-01-08 16:08:09 +0530

    Added gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,32 @@
-var gulp = require('gulp')
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 var babel = require('gulp-babel');
 
+//file path
+var DEST = './build';
+
+//Output both a minified and non-minified version
+gulp.task('js-non-min', function() {
+  return gulp.src('src/js/*.js')
+    // This will output the non-minified version
+    .pipe(babel({ignore: 'gulpfile.js'}))
+    .pipe(gulp.dest(DEST))
+    // This will minify and rename to filename.min.js
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest(DEST));
+});
+
+//ES6 - convert javascript from ES6 to ES5 using Babel
 gulp.task('es6', () => {
-  return gulp.src('./*.js')
+  return gulp.src('src/js/*.js')
       .pipe(babel({ignore: 'gulpfile.js'}))
-      .pipe(gulp.dest('./dist'));
+      .pipe(gulp.dest(DEST));
 });
 
 gulp.task('watch', () => {
-  return gulp.watch('./*.js', ['es6']);
+  return gulp.watch('src/js/*.js', ['es6']);
 });
 
 gulp.task('default', ['es6', 'watch']);
---
+++ src/js/lorem-ipsum.js
@@ -0,0 +1,12 @@
+var odds = evens.map(v => v + 1);
+const nums = evens.map((v, i) => v + i);
+
+let check = 'Developer';
+
+class Hello {
+    constructor() {
+        console.log('World');
+    }
+}
+
+new Hello();
