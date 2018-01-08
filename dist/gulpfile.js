086d661df4925b0482e7c0161249b5b41ef0aa24
SAPIENT\uupad3 <uupadhyay3@sapient.com>
2018-01-08 15:22:55 +0530

    Added gulp task for mocha test

--- dist/gulpfile.js
+++ dist/gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp')
 var babel = require('gulp-babel');
+var mocha = require('gulp-mocha');
+var log = require('gulplog');
 
 gulp.task('es6', () => {
   return gulp.src('./*.js')
       .pipe(babel({ignore: 'gulpfile.js'}))
       .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('mocha', function() {
+  return gulp.src(['test/*.js'], {read: false})
+              .pipe(mocha({reporter: 'list'}))
+              .on('error', log.error)
 });
 
 gulp.task('watch', () => {
